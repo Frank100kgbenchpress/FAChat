3d1290518dbe7a613acbbf9d941376ccaf0b5911
Abel <ryuto1234yolo@gmail.com>
2025-10-17 13:54:44 -0400

    Remove: unnecessary environment variables from docker_script.sh

--- docker/docker_script.sh
+++ docker/docker_script.sh
@@ -22,10 +22,7 @@ echo "🚀 Iniciando contenedor fachat_user1..."
 docker run -d \
   --name fachat_user1 \
   --hostname user1 \
-  --env USER_NAME=Alice \
   --env PORT=5000 \
-  --env MY_MAC=02:42:ac:1c:00:02 \
-  --env SECRET_KEY=c8a6d95a3b912f58e9b2f214f1e3e13f627c04f7a1cb0d8c08d27e7b45e76e90 \
   --env SESSION_COOKIE_NAME=session_app1 \
   --publish 5005:5000 \
   --network $NETWORK_NAME \
@@ -37,10 +34,7 @@ echo "🚀 Iniciando contenedor fachat_user2..."
 docker run -d \
   --name fachat_user2 \
   --hostname user2 \
-  --env USER_NAME=Bob \
   --env PORT=5000 \
-  --env MY_MAC=02:42:ac:1c:00:03 \
-  --env SECRET_KEY=6f88f120cecd5a6eeb1a2d7b92fd1b1434cb4c692a933b2f3ec90bb80e7546e2 \
   --env SESSION_COOKIE_NAME=session_app2 \
   --publish 5006:5000 \
   --network $NETWORK_NAME \
